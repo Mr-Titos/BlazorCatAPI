3f02adfe22c3efdc10d868d561dde4a7a62dcd06
Mr Titos <arthur.titos2@gmail.com>
2023-09-18 19:49:01 +0200

    Ajoutez des fichiers projet.

---
+++ BlazorCatAPI/Modeles/Cat.cs
@@ -0,0 +1,18 @@
+﻿using Newtonsoft.Json;
+
+namespace BlazorCatAPI.Modeles;
+
+public class Cat
+{
+    [JsonProperty("id")]
+    public string Id { get; set; }
+
+    [JsonProperty("url")]
+    public string Url { get; set; }
+
+    [JsonProperty("width")]
+    public int Width { get; set; }
+
+    [JsonProperty("height")]
+    public int Height { get; set; }
+}
---
+++ BlazorCatAPI/Services/CatService.cs
@@ -0,0 +1,29 @@
+﻿using BlazorCatAPI.Modeles;
+using Newtonsoft.Json;
+using RestSharp;
+
+namespace BlazorCatAPI.Services;
+
+public class CatService
+{
+    private static readonly string apiBaseUrl = "https://api.thecatapi.com/v1/";
+    private static RestClient client = new RestClient(apiBaseUrl);
+    private static string apiKey = String.Empty;
+
+    public async Task<Cat?> GetCatImage()
+    {
+        var result = await ExecuteRequest(new RestRequest("images/search", Method.Get));
+        return result.IsSuccessStatusCode ? JsonConvert.DeserializeObject<Cat[]>(result.Content)?.FirstOrDefault() : null;
+    }
+
+    private async Task<RestResponse> ExecuteRequest(RestRequest req)
+    {
+        req.AddParameter("x-api-key", apiKey);
+        return await client.GetAsync(req);
+    }
+
+    public CatService(IConfiguration configuration)
+    {
+        apiKey = configuration["ApiKey"] ?? throw new Exception("Can't retrieve the API key");
+    }
+}
